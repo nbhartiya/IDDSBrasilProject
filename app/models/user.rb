08f772ad504f9c8d817fabe07d4a99e8906bf788
nbhartiya <neeharika@alum.mit.edu>
2012-07-27 11:30:25 -0300

    trying to make messages a subset of users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :dream, :dream_cost, :monthly_savings, :user_name, :user_phone
   validates_uniqueness_of :user_phone
+  has_many :remote_sms
   def to_s
     return "Name: #{user_name}\nPhone: #{user_phone}\n#{dream}: #{dream_cost}\nSaving Rate: #{monthly_savings}"
   end
